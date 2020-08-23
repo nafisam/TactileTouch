4f57c11c0f0f4d57e6ce25d522b19cd47409eae9
nafisam <47230514+nafisam@users.noreply.github.com>
2020-08-23 18:21:42 -0400

    Update MotorController.py

--- Motors/MotorController.py
+++ Motors/MotorController.py
@@ -26,13 +26,18 @@ def energy_function(motor_x, motor_y, touch_x, touch_y):
     x = (motor_x-touch_x)
     if x < -1 or x > 1:
         amplitude = 0
+    if 0.498 < x < 0.503 or -0.498 > x > -0.503:
+        amplitude = -1
     y = (motor_y-touch_y)
+    
+    if 0.498 < y < 0.503 or -0.498 > y > -0.503:
+        amplitude = -1
+    
     beta_x = abs(motor_x-touch_x)/1
     beta_y = abs(motor_y-touch_y)/1
-    if previous_x != 
     if -0.1 < x < 0.1:
         if -0.1 < y < 0.1:
-            amplitude = 100
+            amplitude = 1
     elif -1 < x < 0:
         phantom_h_x = math.sqrt(1-beta_x) * 1
         if y <= 0:
@@ -48,8 +53,9 @@ def energy_function(motor_x, motor_y, touch_x, touch_y):
     
     previous_x = touch_x
     previous_y = touch_y
+    if amplitude == -1:
+        return amplitude = 0
     return amplitude
-
 
         
 def stop_vibration():
@@ -60,22 +66,22 @@ def stop_vibration():
 while True:
     amplitude = [0,0,0,0,0,0,0,0]
     amplitude[0] = energy_function(0, 0, touch_x, touch_y)
-    amplitude[0] = energy_function(1, 0, touch_x, touch_y)
-    amplitude[0] = energy_function(2, 0, touch_x, touch_y)
-    amplitude[0] = energy_function(3, 0, touch_x, touch_y)
-    amplitude[0] = energy_function(0, 1, touch_x, touch_y)
-    amplitude[0] = energy_function(1, 1, touch_x, touch_y)
-    amplitude[0] = energy_function(2, 1, touch_x, touch_y)
-    amplitude[0] = energy_function(3, 1, touch_x, touch_y)
+    amplitude[1] = energy_function(1, 0, touch_x, touch_y)
+    amplitude[2] = energy_function(2, 0, touch_x, touch_y)
+    amplitude[3] = energy_function(3, 0, touch_x, touch_y)
+    amplitude[4] = energy_function(0, 1, touch_x, touch_y)
+    amplitude[5] = energy_function(1, 1, touch_x, touch_y)
+    amplitude[6] = energy_function(2, 1, touch_x, touch_y)
+    amplitude[7] = energy_function(3, 1, touch_x, touch_y)
     
-    motor_00.value = energy_function(0, 0, touch_x, touch_y)
-    motor_10.value = energy_function(1, 0, touch_x, touch_y)
-    motor_20.value = energy_function(2, 0, touch_x, touch_y)
-    motor_30.value = energy_function(3, 0, touch_x, touch_y)
-    motor_01.value = energy_function(0, 1, touch_x, touch_y)
-    motor_11.value = energy_function(1, 1, touch_x, touch_y)
-    motor_21.value = energy_function(2, 1, touch_x, touch_y)
-    motor_31.value = energy_function(3, 1, touch_x, touch_y)
+    motor_00.value = amplitude[0]
+    motor_10.value = amplitude[1]
+    motor_20.value = amplitude[2]
+    motor_30.value = amplitude[3]
+    motor_01.value = amplitude[4]
+    motor_11.value = amplitude[5]
+    motor_21.value = amplitude[6]
+    motor_31.value = amplitude[7]
     
     
     
